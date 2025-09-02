4aa5d1bff1526952c481d712de5a1bebce7a1065
Bennor McCarthy <bennor.mccarthy@vercel.com>
2025-09-02 12:33:58 +1000

    Add Vercel Analytics and Speed Insights to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { VercelToolbar } from "@vercel/toolbar/next";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -39,6 +41,8 @@ export default function RootLayout({
       >
         {children}
         {shouldInjectToolbar && <VercelToolbar />}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
